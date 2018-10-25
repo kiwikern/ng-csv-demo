251c9630b4f3520cca77f3759f68f15efdbc9f61
Kim Kern <github@kimkern.de>
2018-10-25 16:34:52 +0200

    add github page csv example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CsvReaderService } from './csv-reader.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-csv-test';
+
+  constructor(private service: CsvReaderService) {
+  }
+
+  ngOnInit() {
+    this.service.getCsvData()
+      .subscribe(data => this.title = data);
+  }
 }
